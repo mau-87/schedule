3966b3ab6f6842a9725672314eb5dbef2028738c
mau-87 <mauricebrandes@gmx.net>
2020-10-23 21:29:20 +0200

    add many function to create the table header and draw into element

--- app/js/schedule.js
+++ app/js/schedule.js
@@ -1,7 +1,8 @@
 var Schedule = function() {
 	this.time_range = {
 		startHour: 0,
-		endHour: 23
+		endHour: 23,
+		timeDivision: 15
 	};
 	
 	this.date_range = {
@@ -9,6 +10,11 @@ var Schedule = function() {
 		endDate: new Date('1970-01-01'),	
 	};
 	
+	this.table = {
+		colSpanDate: 96,
+		colSpanHour: 4
+	}
+
 	this.data = [];
 	this.DiffDays();
 };
@@ -23,14 +29,14 @@ Schedule.prototype = {
 	setTime: function(startHour,endHour) {
 		if(this.isValidHour(startHour) && this.isValidHour(endHour)) {
 			if(this.isValidTimeRange(startHour, endHour)) {
-				this.startHour = startHour;
-				this.endHour = endHour;
+				this.time_range.startHour = startHour;
+				this.time_range.endHour = endHour;
 			} else {
-				throw new Error('the start hour must less the end hour');
+				throw new Error('the start hour must less the end hour!');
 			}
 			
 		} else {
-			throw new Error('function setTime need a number between 0 and 23');
+			throw new Error('function setTime need a number between 0 and 23!');
 		}
 	},
 
@@ -39,7 +45,7 @@ Schedule.prototype = {
 	},
 
 	isValidHour: function(hour) {
-		return hour >= 0 && hour < 23;
+		return hour >= 0 && hour <= 23;
 	},
 
 	setDate: function(startDate, endDate) {
@@ -50,10 +56,10 @@ Schedule.prototype = {
 
 				this.DiffDays();
 			} else {
-				throw new Error('the start date must earlier or equals to the end date');
+				throw new Error('the start date must earlier or equals to the end date!');
 			}
 		} else {
-			throw new Error('function setDate need a date');
+			throw new Error('function setDate need a date!');
 		}
 	},
 
@@ -63,78 +69,113 @@ Schedule.prototype = {
 
 	isValidDateRange: function(startDate, endDate) {
 		return startDate.getTime() <= endDate.getTime();
+	},
+
+	setTimeDivision: function(timeDivision) {
+		if (Number.isInteger(60 / timeDivision)) {
+			this.time_range.timeDivision = timeDivision;
+		} else {
+			throw new Error('Please enter a correct number!');
+		}
+		
+	},
+
+	setColSpanDate: function() {
+		this.table.colSpanDate = ((this.time_range.endHour - this.time_range.startHour) + 1) * this.table.colSpanHour;
+	},
+
+	setColSpanHour: function() {
+		this.table.colSpanHour = 60 / this.time_range.timeDivision;
+	},
+
 	}
+
 };
 
 Schedule.Renderer = function(s) {
 	if (!(s instanceof Schedule)) {
-		throw new Error('Is not a Object of Schedule');
+		throw new Error('Is not a Object of schedule!');
 	}
-	this.Schedule = s;
+	this.schedule = s;
 };
 
 Schedule.Renderer.prototype = {
 	draw: function(selector) {
 		var schedule = this.schedule;
 
+		schedule.setColSpanHour();
+		schedule.setColSpanDate();
+
 		var container = document.querySelector(selector);
 
-		// var scopeDurationHours = getDurationHours(timetable.scope.hourStart, timetable.scope.hourEnd);
 		var divDesignation = appendDivDesignation(container);
 		var divTimeline = appendDivTimeline(container);
 	
 
 		function appendDivDesignation(container) {
-		div = container.appendChild(document.createElement('div'));
-		div.id = "designation"
-		div.className = 'col-25';
-		return appendTableDesignation(div);
-	}
+			div = container.appendChild(document.createElement('div'));
+			div.id = "designation"
+			div.className = 'col-25';
+			return appendTableDesignation(div);
+		}
 
-	function appendTableDesignation(div) {
-		table = div.appendChild(document.createElement('table'));
-		table.id = "table_designation"
-		return appendTableTrHeaderDesignation(table);
-	}
+		function appendTableDesignation(div) {
+			table = div.appendChild(document.createElement('table'));
+			table.id = "table_designation"
+			return appendTableTrHeaderDesignation(table);
+		}
 
-	function appendTableTrHeaderDesignation(table) {
-		for (let index = 0; index < 2; index++) {
+		function appendTableTrHeaderDesignation(table) {
+			for (let index = 0; index < 2; index++) {
+				var tr = table.appendChild(document.createElement('tr'));
+				var td = document.createElement('td');
+
+				td.appendChild(document.createTextNode('\u00A0'));
+				tr.appendChild(td);
+				
+			}		
+		}
+
+		function appendDivTimeline(container) {
+			div = container.appendChild(document.createElement('div'));
+			div.id = "timeline"
+			div.className = 'col-75';
+			return appendTableTimeline(div);
+		}
+
+		function appendTableTimeline(div) {
+			table = div.appendChild(document.createElement('table'));
+			table.id = "table_timeline"
+			return appendTableTrHeaderTimeline(table);
+		}
+
+		function appendTableTrHeaderTimeline(table) {
 			var tr = table.appendChild(document.createElement('tr'));
-			var td = document.createElement('td');
+			var date = schedule.date_range.startDate;
 
-			td.appendChild(document.createTextNode('\u00A0'));
-			tr.appendChild(td);
+			for (let index = 0; index <= schedule.date_range.days; index++) {
+				var th = document.createElement('th');
+
+				th.colSpan = schedule.table.colSpanDate;
+				th.appendChild(document.createTextNode(date.toLocaleDateString()));
+				tr.appendChild(th);
+				
+				date.setDate(date.getDate() +1);
+			}	
 			
-		}		
-	}
-
-	function appendDivTimeline(container) {
-		div = container.appendChild(document.createElement('div'));
-		div.id = "timeline"
-		div.className = 'col-75';
-		return appendTableTimeline(div);
-	}
-
-	function appendTableTimeline(div) {
-		table = div.appendChild(document.createElement('table'));
-		table.id = "table_timeline"
-		return appendTableTrHeaderTimeline(table);
-	}
-
-	function appendTableTrHeaderTimeline(table) {
-		var tr = table.appendChild(document.createElement('tr'));
-		var date = this.schedule.date_range.startDate;
-
-		for (let index = 0; index <= this.schedule.date_range.days; index++) {
-			var td = document.createElement('th');
-
-			td.appendChild(document.createTextNode(date.toLocaleDateString()));
-			tr.appendChild(td);
+			tr = table.appendChild(document.createElement('tr'));
 			
-			date.setDate(date.getDate() +1);
-		}		
-	}
+			for (let date_counter = 0; date_counter <= schedule.date_range.days; date_counter++) {
+				for (let index = schedule.time_range.startHour; index <= schedule.time_range.endHour; index++) {
+					var td = document.createElement('td');
+					
+					td.colSpan = schedule.table.colSpanHour;
+					td.appendChild(document.createTextNode(index+' Uhr'));
+					tr.appendChild(td);
+					
+				}
+			}
+		}
 
 	}
 };
-
